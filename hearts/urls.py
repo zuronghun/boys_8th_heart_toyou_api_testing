26724aa29a9a8946fdf9306ca83dc0a36d8d3057
00ZRHun <00zrhun@gmail.com>
2023-04-06 18:01:49 +0800

    feat: add V1 api for hearts

--- hearts/urls.py
+++ hearts/urls.py
@@ -4,5 +4,6 @@ from hearts import views
 urlpatterns = [
     re_path(r'^api/hearts$', views.heart_list),
     re_path(r'^api/hearts/(?P<pk>[0-9]+)$', views.heart_detail),
-    # re_path(r'^api/v1/hearts$', views.heart_list_v1)
+    re_path(r'^api/v1/hearts$', views.heart_list_v1),
+    re_path(r'^api/v1/hearts/(?P<total>[0-9]+)$', views.heart_list_v1),
 ]
--- hearts/views.py
+++ hearts/views.py
@@ -81,84 +81,126 @@ def heart_detail(request, pk):
 # v1 api for getHeartByTerm
 
 
-# @api_view(['GET', 'POST'])   # , 'DELETE'
-# def heart_list_v1(request):
-#     # permission_classes = [AllowAny] # TODO
-#     # GET list of hearts, POST a new heart, DELETE all hearts
+@api_view(['GET', 'POST'])
+def heart_list_v1(request, total=None):
+    pk = 1   # assume only one data
+    # find heart by pk (id)
+    try:
+        heart = Heart.objects.get(pk=pk)
+    except Heart.DoesNotExist:
+        return JsonResponse({'message': 'The heart does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-#     # retrieve objects (with condition)
-#     # retrieve all Hearts / find by term from PostgreSQL database
-#     if request.method == 'GET':
-#         hearts = Heart.objects.all()
+    # retrieve a single object
+    # find a single Heart with an id
+    if request.method == 'GET':
+        heart_serializer = HeartSerializers(heart)
+        return JsonResponse(heart_serializer.data)
 
-#         term = request.GET.get('term', None)
-#         if term is not None:
-#             # complate value (exactly same), else recreate new obj
-#             # OPT: term__icontains=term
-#             hearts = hearts.filter(term=term)
+    elif request.method == 'POST':
+        print("=== HERE ===")
+        print(f"request = {request}")
+        print(f"total = {total}")
+        # total = request.GET.get('total', None)
+        # print(f"total = {total}")
 
-#             # if empty, then create a new obj
-#             # IN
-#             # print(f"hearts = {hearts}")
-#             # print(f"hearts.__sizeof__ = {hearts.__sizeof__}")
-#             # print(f"len(hearts) = {len(hearts)}")
-#             # print(f"term = {term}")
-#             # OUT
-#             # hearts = <QuerySet []>
-#             # hearts.__sizeof__ = <built-in method __sizeof__ of QuerySet object at 0x7fd6dd9105e0>
-#             # len(hearts) = 0
-#             # term = あんスタ
-#             if len(hearts) < 1:
-#                 # create a new object
-#                 # create and save a new Heart
+        if total is not None:
+            # TODO: heart_data = {"total": total}
+            heart_data = dict()
+            currTotal = getattr(heart, "total")
+            heart_data["total"] = currTotal + int(total)
+            print(f"heart_data = {heart_data}")
 
-#                 # heart_data = JSONParser().parse(request)
-#                 heart_data = dict()
-#                 # OPT: #あんスタ, #あんスタウェルカム祭, #はじめてさんいらっしゃ〜い
-#                 data = get_heart_text(None, term, "ja")
-#                 heart_data["term"] = term
-#                 heart_data["data"] = data
-#                 # print(f"heart_data = {heart_data}")   # D
+            # serialize & save into db
+            heart_serializer = HeartSerializers(
+                heart, data=heart_data)   # type: ignore
+            print(f"heart_serializer = {heart_serializer}")
 
-#                 heart_serializer = HeartSerializers(
-#                     data=heart_data)  # type: ignore
-#                 # print(f"heart_serializer = {heart_serializer}")   # D
+            if (heart_serializer.is_valid()):
+                heart_serializer.save()
+                return JsonResponse({'message': 'Heart total is updated successfully!'}, status=status.HTTP_204_NO_CONTENT)
+            return JsonResponse(heart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#                 if (heart_serializer.is_valid()):
-#                     heart_serializer.save()
-#                     return JsonResponse(heart_serializer.data, status=status.HTTP_201_CREATED)
-#                 return JsonResponse(heart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return JsonResponse({'message': 'total field is required!'}, status=status.HTTP_400_BAD_REQUEST)
+    # TODO: else:
 
-#             hearts_serializers = HeartSerializers(hearts, many=True)
+    ######
+    ######
+    ######
+    # # GET list of hearts, POST a new heart all hearts
 
-#             # TODO: CAN't WORK: hearts_serializers.data["data"] / .get()
-#             # print(f"hearts_serializers = {hearts_serializers}")
+    # # retrieve objects (with condition)
+    # # retrieve all Hearts / find by term from PostgreSQL database
+    # if request.method == 'GET':
+    #     hearts = Heart.objects.all()
 
-#             # print(f'hearts = {hearts}')
-#             # print(f'getattr(hearts, "data") = {getattr(hearts, "data")}')
-#             return JsonResponse(hearts_serializers.data, safe=False)
-#             # 'safe=False' for objects serialization
+    #     # term = request.GET.get('term', None)
+    #     # if term is not None:
+    #     #     # complate value (exactly same), else recreate new obj
+    #     #     # OPT: term__icontains=term
+    #     #     hearts = hearts.filter(term=term)
 
-#         else:
-#             for heart in hearts:
-#                 # refresh & update single heart by its term
-#                 print("=== cron job is auto running ===")
-#                 term = getattr(heart, "term")   # OPT: heart["term"]
+    #     #     # if empty, then create a new obj
+    #     #     # IN
+    #     #     # print(f"hearts = {hearts}")
+    #     #     # print(f"hearts.__sizeof__ = {hearts.__sizeof__}")
+    #     #     # print(f"len(hearts) = {len(hearts)}")
+    #     #     # print(f"term = {term}")
+    #     #     # OUT
+    #     #     # hearts = <QuerySet []>
+    #     #     # hearts.__sizeof__ = <built-in method __sizeof__ of QuerySet object at 0x7fd6dd9105e0>
+    #     #     # len(hearts) = 0
+    #     #     # term = あんスタ
+    #     #     if len(hearts) < 1:
+    #     #         # create a new object
+    #     #         # create and save a new Heart
 
-#                 heart_data = dict()
-#                 # data = {"num666": 123666}
-#                 data = get_heart_text(None, term, "ja")
-#                 heart_data["term"] = term
-#                 heart_data["data"] = data
+    #     #         # heart_data = JSONParser().parse(request)
+    #     #         heart_data = dict()
+    #     #         # OPT: #あんスタ, #あんスタウェルカム祭, #はじめてさんいらっしゃ〜い
+    #     #         data = get_heart_text(None, term, "ja")
+    #     #         heart_data["term"] = term
+    #     #         heart_data["data"] = data
+    #     #         # print(f"heart_data = {heart_data}")   # D
 
-#                 # serialize & save into db
-#                 heart_serializer = HeartSerializers(
-#                     heart, data=heart_data)  # type: ignore
-#                 # heart_serializer.
-#                 if (heart_serializer.is_valid()):
-#                     heart_serializer.save()
-#                     return JsonResponse({'message': 'All hearts were updated by term successfully!'}, status=status.HTTP_204_NO_CONTENT)
-#                 return JsonResponse(heart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     #         heart_serializer = HeartSerializers(
+    #     #             data=heart_data)  # type: ignore
+    #     #         # print(f"heart_serializer = {heart_serializer}")   # D
+
+    #     #         if (heart_serializer.is_valid()):
+    #     #             heart_serializer.save()
+    #     #             return JsonResponse(heart_serializer.data, status=status.HTTP_201_CREATED)
+    #     #         return JsonResponse(heart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #     #     hearts_serializers = HeartSerializers(hearts, many=True)
+
+    #     #     # TODO: CAN't WORK: hearts_serializers.data["data"] / .get()
+    #     #     # print(f"hearts_serializers = {hearts_serializers}")
+
+    #     #     # print(f'hearts = {hearts}')
+    #     #     # print(f'getattr(hearts, "data") = {getattr(hearts, "data")}')
+    #     #     return JsonResponse(hearts_serializers.data, safe=False)
+    #     #     # 'safe=False' for objects serialization
+
+    #     # else:
+    #     #     for heart in hearts:
+    #     #         # refresh & update single heart by its term
+    #     #         print("=== cron job is auto running ===")
+    #     #         term = getattr(heart, "term")   # OPT: heart["term"]
+
+    #     #         heart_data = dict()
+    #     #         # data = {"num666": 123666}
+    #     #         data = get_heart_text(None, term, "ja")
+    #     #         heart_data["term"] = term
+    #     #         heart_data["data"] = data
+
+    #     #         # serialize & save into db
+    #     #         heart_serializer = HeartSerializers(
+    #     #             heart, data=heart_data)  # type: ignore
+    #     #         # heart_serializer.
+    #     #         if (heart_serializer.is_valid()):
+    #     #             heart_serializer.save()
+    #     #             return JsonResponse({'message': 'All hearts were updated by term successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    #     #         return JsonResponse(heart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # # _v1 Reusable / small function(s)
